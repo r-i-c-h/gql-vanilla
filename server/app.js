909ddb7544fb178a2cc1eff5db5055d5b21d1e97
r-i-c-h <rich@richwerden.com>
2019-01-18 23:15:23 -0800

    Adds MongoDB connection and dot.env for keys

--- server/app.js
+++ server/app.js
@@ -1,21 +1,27 @@
+/*eslint-disable*/
+require('dotenv').config({ path: '../.env' });
 const express = require('express');
 const graphqlHTTP = require('express-graphql'); // No, it doesn't match the package name...
+const mongoose = require('mongoose');
 const schema = require('./schema/schema');
-
-const PORT = 7777;
+const PORT = process.env.PORT;
+const mongoUser = process.env.MLAB_USER;
+const mongoPass = process.env.MLAB_PASS;
 
 const app = express();
-/*eslint-disable*/
-// bind express with graphql
-app.use(
-  '/graphql',
-  // Respond to requests to localhost:PORT/graphql
+
+mongoose.connect(`mongodb://${mongoUser}:${mongoPass}@ds161074.mlab.com:61074/gql-data-src`,  {useNewUrlParser: true});
+mongoose.connection.once('open', ()=> { // on success callback func()
+  console.log('Connected to mLab Database');
+})
+
+app.use( '/graphql', // Respond to requests to localhost:PORT/graphql
   graphqlHTTP({
     schema,
-    graphiql: true
+    graphiql: true // allow/activate graphiql interface
   })
 );
 
 app.listen(PORT, () => {
-  console.log(`Server up listening for requests on port http://localhost:${PORT}`);
+  console.log(`Server up listening for requests on http://localhost:${PORT}`);
 });
