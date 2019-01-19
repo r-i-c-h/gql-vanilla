98edf2a40680435d0680a76acd07011ce7b99afe
r-i-c-h <rich@richwerden.com>
2019-01-19 02:36:44 -0800

    Hooks Query/Mutation Resolvers to M-Lab

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -3,22 +3,21 @@
 const graphql = require('graphql');
 const Book = require('../mongo-models/book');
 const Author = require('../mongo-models/author');
-
 const {
   GraphQLID,
   GraphQLInt,
   GraphQLList,
   GraphQLObjectType,
   GraphQLSchema,
-  GraphQLString
+  GraphQLString,
+  GraphQLNonNull
 } = graphql;
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 /*** Describe Object Types & Relationships ***/
+// Kinda like Models, but technically not db-models, just gQL stuff...
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
-    // A func because we want a returned object
-    // Only funcs can describe Type-Relations
     id: { type: GraphQLID }, // gQL ID is essentially type:any - really cast to String
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
@@ -28,8 +27,7 @@ const BookType = new GraphQLObjectType({
       // extend the BookType
       type: AuthorType,
       resolve(parent, args) { // `parent` is the Book
-        // const ans = authorz.filter(x => x.id === parent.authorId);
-        // return ans[0];
+        return Author.findById(parent.authorId);
       }
     }
   })
@@ -44,61 +42,56 @@ const AuthorType = new GraphQLObjectType({
     books: {
       type: new GraphQLList(BookType), /** Many to One Relationship **/
       resolve(parent, args) {
-        // return bookz.filter(book => {
-        //   if (book.authorId === parent.id) return book;
-        // });
+        return Book.find({authorId: parent.id});
       }
     }
   })
 });
-
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-/*** Define All Queries Within RootQuery ***/
+/*** QUERY DEFs: Define All Queries Within RootQuery ***/
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
     book: { // "book" = Name of query
-      // expect request to be `book(id:'123'){ /*....*/ }
-      type: BookType, // Expected return type
+      type: BookType, // Expected return type as defined above
       args: { id: { type: GraphQLID } }, // Expected Arguments
-  /*** HOW to Get the Data from the 'book' query ***/
       resolve(parent, args) {
-        // const ans = bookz.filter(x => x.id === args.id);
-        // return ans[0]; // gQL wants an Obj{} response, not Arr[]
+/*** HOW to Get the Data ***/
+        return Book.findById(args.id);
       }
     },
     author: { // "author" query
       type: AuthorType, // return an AuthorType as defined above
       args: { id: { type: GraphQLID } }, // Expected Arguments
       resolve(parent, args) {
-        // const ans = authorz.filter(writer => writer.id === args.id);
-        // return ans[0];
+        return Author.findById(args.id);
       }
     },
     books: { // SELECT ALL
       type: new GraphQLList(BookType),
       resolve(parent,args){
-        // return bookz;
+        return Book.find({}); // Mongoose .find w/out args (empty obj{}) matches all.
       }
     },
     authors: { // SELECT ALL
       type: new GraphQLList(AuthorType),
       resolve(parent,args){
-        // return authorz;
+        return Author.find({});
       }
     }
   }
 });
-
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+/*** MUTATION Add\Del DEFs ***/
 const Mutation = new GraphQLObjectType({
   name: 'Mutation',
   fields: {
     addNewBook: {
       type: BookType,
       args: {
-        name: { type: GraphQLString },
+        name: { type: new GraphQLNonNull(GraphQLString) },
         genre: { type: GraphQLString },
-        authorId: { type: GraphQLID }
+        authorId: { type: new GraphQLNonNull(GraphQLID) }
       },
       resolve(parent, args){
         let newBookInstance = new Book({ // Book() is from the mongoose-model
@@ -106,13 +99,13 @@ const Mutation = new GraphQLObjectType({
           genre: args.genre,
           authorId: args.authorId
         });
-        return newBookInstance.save(); // Part of MongoDB is that it will return whatever was .saved()
+        return newBookInstance.save(); // MongoDB's .save() returns whatever was saved
       }
     },
     addNewAuthor: {
       type: AuthorType,
       args: {
-        name: { type: GraphQLString },
+        name: { type: new GraphQLNonNull(GraphQLString) },
         age: { type: GraphQLInt }
       },
       resolve(parent, args){
