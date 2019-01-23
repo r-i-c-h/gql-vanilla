8ea61dc34d82c029c2f97dc447a6fc03ce38f9c0
r-i-c-h <rich@richwerden.com>
2019-01-23 02:20:35 -0800

    Adds cors package to server to allow for reqs back from m-lab

--- gql-server/app.js
+++ gql-server/app.js
@@ -3,13 +3,15 @@ require('dotenv').config({ path: '../.env' });
 const express = require('express');
 const graphqlHTTP = require('express-graphql'); // No, it doesn't match the package name...
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 const schema = require('./schema/schema');
 const PORT = process.env.PORT;
 const mongoUser = process.env.MLAB_USER;
 const mongoPass = process.env.MLAB_PASS;
 
 const app = express();
-
+app.use(cors());
 mongoose.connect(`mongodb://${mongoUser}:${mongoPass}@ds161074.mlab.com:61074/gql-data-src`,  {useNewUrlParser: true});
 mongoose.connection.once('open', ()=> { // on success callback func()
   console.log('Connected to mLab Database');
